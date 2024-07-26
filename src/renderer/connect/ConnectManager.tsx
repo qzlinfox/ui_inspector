6152128796a500a7b98fd850d10d4dcd6e0d5d5c
q_zlin <q_zlin@foxmail.com>
2024-07-26 14:38:29 +0800

    feat: 设备列表页面内容

--- src/renderer/connect/ConnectManager.tsx
+++ src/renderer/connect/ConnectManager.tsx
@@ -1,5 +1,32 @@
+import { TabPane, Tabs } from '@douyinfe/semi-ui';
+
 export default function ConnectManager() {
-  return(
-    <>连接管理</>
-  )
+
+  const state = {
+    tabList: [
+      { tab: 'HNNTH', itemKey: '00008110-001E1C843E61801E', text: 'HNNTH', closable: true },
+      { tab: 'qiuzilin的iPhone (2)', itemKey: 'AKRSVB1731011612', text: 'qiuzilin的iPhone (2)', closable: true },
+    ],
+  };
+
+  const connects = {
+    '00008110-001E1C843E61801E': {
+      name: 'qiuzilin的iPhone (2)',
+      screenShot: '',
+      nodeTree: {}
+    },
+    'AKRSVB1731011612': {
+
+    }
+  }
+
+  return (
+    <Tabs type="card">
+      {state.tabList.map(t => (
+        <TabPane closable={t.closable} tab={t.tab} itemKey={t.itemKey} key={t.itemKey}>
+          {t.text}
+        </TabPane>
+      ))}
+    </Tabs>
+  );
 }
